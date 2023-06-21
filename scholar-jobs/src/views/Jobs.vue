400491dd45376b0b9053da4078459dd744dd5f64
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-21 12:29:43 +0200

    Update Jobs.vue

--- scholar-jobs/src/views/Jobs.vue
+++ scholar-jobs/src/views/Jobs.vue
@@ -1,10 +1,21 @@
 <template>
-
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }}</h2>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            jobs: [
+                {title: 'Scholar UX Designer', id: 1, details: 'lorem'},
+                {title: 'Scholar Web Developer', id: 2, details: 'lorem'},
+                {title: 'Scholar Vue Developer', id: 3, details: 'lorem'},
+            ]
+        }
+    }
 }
 </script>
 
